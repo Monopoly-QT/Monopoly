1f89edef25c2ad3d89c709550b5ce67d7eda3235
Kai <imatch181@gmail.com>
2025-05-20 16:12:09 +0800

    Add .gitignore

--- build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/appmonopoly_qml.cmake
+++
@@ -1,9 +0,0 @@
-
-set(target "appmonopoly")
-set(working_dir "C:/Users/user/Desktop/OOP_project/MonopolyUI/monopoly")
-set(src_and_dest_list
-    "C:/Users/user/Desktop/OOP_project/MonopolyUI/monopoly/Main.qml"
-    "C:/Users/user/Desktop/OOP_project/MonopolyUI/monopoly/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/monopoly/Main.qml"
-
-)
-set(timestamp_file "C:/Users/user/Desktop/OOP_project/MonopolyUI/monopoly/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/appmonopoly_qml.txt")
