f4ec608a6c2b458227a10389306972a06dcdd635
XuZero0426 <ethanxu0317@gmail.com>
2025-05-28 13:57:39 +0800

    bruh

--- Handler/EventHandler/eventHandler.cpp
+++ Handler/EventHandler/eventHandler.cpp
@@ -920,7 +920,7 @@ void eventHandler::toll() {
     int fee = (nowLevel) * (landValue / 10);
     processPlayer[turn]->subMoney(fee);
     processPlayer[landOwner]->addMoney(fee);
-    popUpdisplaySetting("-" + to_string(fee), 0);
+    popUpdisplaySetting("You passed through the area of " + processPlayer[turn]->getPlayerName()+"\nand paid " +to_string(fee)+" dollars to "+processPlayer[landOwner]->getPlayerName(), 0);
     nextTurn();
 }
 
@@ -960,7 +960,7 @@ void eventHandler::levelup() {
 
 void eventHandler::sellLand() {
     int nowPos = processPlayer[turn]->getPos();
-    int value = processMap[nowPos]->getValue();
+    int value = processMap[nowPos]->getValue() * 0.8;
     processPlayer[turn]->addMoney((value / 2) + (processMap[nowPos]->getLevel() * value / 2));
     processMap[nowPos]->setOwner(-1);
     processMap[nowPos]->setLevel(0);
